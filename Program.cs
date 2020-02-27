ee05812d2baea4f5d1aa0a2093025e10848dbe8b
Paul Oliver <Pauliver@gmail.com>
2020-02-26 19:30:54 -0800

    a bit more troubleshoting

--- Program.cs
+++ Program.cs
@@ -29,75 +29,74 @@ namespace Merge_Pull_Request
                 Owner = args[1] as string;
             }
             var AutoMergeLabel = "automerge";
-            if(aargs[2] is string)
+            if(args[2] is string)
             {
                 AutoMergeLabel = args[2] as string;
             }
 
+            GitHubClient github = null;
+            bool LoggedIn = false;
+
             // come back and make every arg beyond 3 also a valid label maybe?
             
-            if(true)
-            {
-                try{
-                    Console.WriteLine("Loading github...");
-                    string secretkey = Environment.GetEnvironmentVariable("GITHUB_TOKEN");
-                    github = new GitHubClient(new ProductHeaderValue("Pauliver-ImageTool"))
-                    {
-                        Credentials = new Credentials(secretkey)
-                    };
-                    CleanlyLoggedIn = true; // or maybe
-                    Console.WriteLine("... Loaded");
-                }catch(Exception ex){
-                    Console.WriteLine(ex.ToString());
-                    Console.WriteLine("... Loading Failed");
-                    CleanlyLoggedIn = false;
-                }
+            try{
+                Console.WriteLine("Loading github...");
+                string secretkey = Environment.GetEnvironmentVariable("GITHUB_TOKEN");
+                github = new GitHubClient(new ProductHeaderValue("Pauliver-ImageTool"))
+                {
+                    Credentials = new Credentials(secretkey)
+                };
+                LoggedIn = true; // or maybe
+                Console.WriteLine("... Loaded");
+            }catch(Exception ex){
+                Console.WriteLine(ex.ToString());
+                Console.WriteLine("... Loading Failed");
+                LoggedIn = false;
             }
+            
 
             Console.WriteLine(" --- ");
 
-            if(GitHubStuff)
+            if(!CleanlyLoggedIn)
             {
-                if(!CleanlyLoggedIn)
-                {
-                    Console.WriteLine("GitHub Login State unclear, Exiting");
-                    return;
-                }
-                
-                string PRname = "From " + CurrentBranch + " to " + GHPages;
-                
-                bool shouldmerge = false;
+                Console.WriteLine("GitHub Login State unclear, Exiting");
+                return;
+            }
+            
+            string PRname = "From " + CurrentBranch + " to " + GHPages;
+            
+            bool shouldmerge = false;
 
-                var prs = await github.PullRequest.GetAllForRepository(Owner,Repo);
+            var prs = await github.PullRequest.GetAllForRepository(Owner,Repo);
                 
-                foreach(PullRequest pr in prs)
+            foreach(PullRequest pr in prs)
+            {
+                foreach(Label l in pr.Labels)
                 {
-                    foreach(Label l in pr.Labels)
+                    if(l.Name == AutoMergeLabel)
                     {
-                        if(l.Name == AutoMergeLabel)
+                        shouldmerge = true;
+                    }
+                    if(false && pr.Title.Contains(PRname))
+                    {
+                        shouldmerge = true;
+                    }
+                    if(shouldmerge)
+                    {
+                        MergePullRequest mpr = new MergePullRequest();
+                        mpr.CommitMessage = "Time's up, let's do this";
+                        mpr.MergeMethod = PullRequestMergeMethod.Merge;
+                        
+                        var merge = await github.PullRequest.Merge(Owner,Repo,pr.Number,mpr);
+                        if(merge.Merged)
                         {
-                            shouldmerge = true;
-                        }
-                        if(false && pr.Title.Contains(PRname))
-                        {
-                            shouldmerge = true;
-                        }
-                        if(shouldmerge)
-                        {
-                            MergePullRequest mpr = new MergePullRequest();
-                            mpr.CommitMessage = "Time's up, let's do this";
-                            mpr.MergeMethod = PullRequestMergeMethod.Merge;
-                            
-                            var merge = await github.PullRequest.Merge(Owner,Repo,pr.Number,mpr);
-                            if(merge.Merged)
-                            {
-                                Console.WriteLine("Successfully Merged");
-                            }else{
-                                Console.WriteLine("Merge Failed");
-                            }
+                            Console.WriteLine("Successfully Merged");
+                        }else{
+                            Console.WriteLine("Merge Failed");
                         }
                     }
                 }
+            
                 Console.WriteLine("And we are done here");
             }
         }
